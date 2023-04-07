44afcb0c4e6340802facc24de48618346e834f9f
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-04-08 01:45:49 +0900

    Docs : 230407 Variable 자습

---
+++ 230407/Variable.js
@@ -0,0 +1,18 @@
+//Variable Declare(변수 선언)
+let a; //(= var a;)
+console.log(a); //undefined
+
+//Initialize(초기화)
+a = 1;
+
+//선언과 초기화를 동시에 할 수 있음.
+let radius = 2; 
+let pi = 3.14;
+let area = radius * radius * pi;
+console.log(area); // 12.56
+
+var firstName = "YoungJin";
+var lastName = "An";
+console.log(firstName + lastName); // YoungJin An
+
+
