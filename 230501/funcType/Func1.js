207397b653a755050f4afb3f48fb49b233fcedb7
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-05-02 00:17:52 +0900

    📝 Docs : 230501 Component(Function Type) 자습

---
+++ 230501/funcType/Func1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Func1 = (props) => {
+    return ( 
+        <div>
+            <header>
+                <h1>함수형 컴포넌트 1</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Func1;
