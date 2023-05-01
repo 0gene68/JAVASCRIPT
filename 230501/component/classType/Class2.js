32c02249bb4483b3a4af08ac1907ddc7a9f63f9a
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-05-02 00:17:25 +0900

    📝 Docs : 230501 Component(Class Type) 자습

---
+++ 230501/component/classType/Class2.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Class2 extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <h2>클래스형 컴포넌트 2</h2>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Class2;
